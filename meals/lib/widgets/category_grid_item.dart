d0fb4dd6a7249306afb09184b727f67ff458b9ff
leangsengLy <edeepleang@gmail.com>
2024-12-22 15:56:15 +0700

    update new project

---
+++ meals/lib/widgets/category_grid_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem(this.category, {super.key});
+  final Category category;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
