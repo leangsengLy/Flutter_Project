df1f3909ac22ba681a0fa5892754b7299e518c9d
leangsengLy <edeepleang@gmail.com>
2025-01-04 07:51:31 +0700

    add new

--- favorite_place/lib/main.dart
+++ favorite_place/lib/main.dart
@@ -4,16 +4,32 @@ import 'package:google_fonts/google_fonts.dart';
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
   seedColor: const Color.fromARGB(255, 102, 6, 247),
-  onBackground: const Color.fromARGB(255, 56, 49, 66),
 );
 final theme = ThemeData().copyWith(
-    useMaterial3: true,
-    scaffoldBackgroundColor: const Color.fromARGB(255, 56, 49, 66),
-    colorScheme: colorScheme,
-    textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
-        titleSmall: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
-        titleMedium: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
-        titleLarge: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold)));
+  scaffoldBackgroundColor: const Color.fromARGB(255, 56, 49, 66),
+  colorScheme: colorScheme,
+  textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
+    titleSmall: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+    titleMedium: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+    titleLarge: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+  ),
+);
 void main() {
-  runApp(MaterialApp());
+  runApp(const AppProject());
+}
+
+class AppProject extends StatelessWidget {
+  const AppProject({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      title: "Grade place",
+      home: const Scaffold(
+        body: Center(
+          child: Text("Hello world"),
+        ),
+      ),
+    );
+  }
 }
