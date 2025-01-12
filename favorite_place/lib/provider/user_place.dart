07cb116441340a9fe37f7ab75b77cfbfa7368892
leangsengLy <edeepleang@gmail.com>
2025-01-12 11:08:59 +0700

    update new

--- favorite_place/lib/provider/user_place.dart
+++ favorite_place/lib/provider/user_place.dart
@@ -1,12 +1,61 @@
 import 'dart:io';
 import 'package:favorite_place/models/place..dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 import 'package:riverpod/riverpod.dart';
 
+Future<Database> getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY,title TEXT,image TEXT,lat REAL,lng REAL,address TEXT)');
+    },
+    version: 1,
+  );
+  return db;
+}
+
 class UserPlactNotifier extends StateNotifier<List<Place>> {
+  Future<void> loadPlaces() async {
+    final db = await getDatabase();
+    final data = await db.query('user_places');
+    final places = data
+        .map(
+          (row) => Place(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            placeLocation: PlaceLocation(
+              latitude: row['lat'] as double,
+              longitude: row['lng'] as double,
+              address: row['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+    state = places;
+  }
+
   UserPlactNotifier() : super(const []);
-  void addPlace(String title, File image, PlaceLocation placeLocation) {
+  void addPlace(String title, File image, PlaceLocation placeLocation) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copyImage = await image.copy('${appDir.path}/$filename');
     final newPlace =
-        Place(title: title, image: image, placeLocation: placeLocation);
+        Place(title: title, image: copyImage, placeLocation: placeLocation);
+    final db = await getDatabase();
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.placeLocation.latitude,
+      'lng': newPlace.placeLocation.longitude,
+      'address': newPlace.placeLocation.address,
+    });
     state = [...state, newPlace];
   }
 }
--- favorite_place/lib/screens/places.dart
+++ favorite_place/lib/screens/places.dart
@@ -4,8 +4,22 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:favorite_place/widget/places_list.dart';
 import 'package:flutter/material.dart';
 
-class PlacesScreen extends ConsumerWidget {
+class PlacesScreen extends ConsumerStatefulWidget {
   const PlacesScreen({super.key});
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    return PlacesScreenState();
+  }
+}
+
+class PlacesScreenState extends ConsumerState<PlacesScreen> {
+  late Future<void> placesFuture;
+  @override
+  void initState() {
+    super.initState();
+    placesFuture = ref.read(UserPlaceProviders.notifier).loadPlaces();
+  }
+
   void onClickAddNewPlace(BuildContext context) {
     Navigator.of(context).push(
       MaterialPageRoute(
@@ -15,7 +29,7 @@ class PlacesScreen extends ConsumerWidget {
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final listPlace = ref.watch(UserPlaceProviders);
     return Scaffold(
       appBar: AppBar(
@@ -31,8 +45,16 @@ class PlacesScreen extends ConsumerWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: PlacesList(
-          places: listPlace,
+        child: FutureBuilder(
+          future: placesFuture,
+          builder: (context, snapshot) =>
+              snapshot.connectionState == ConnectionState.waiting
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : PlacesList(
+                      places: listPlace,
+                    ),
         ),
       ),
     );
